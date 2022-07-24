5a93726e28346941e08fba9b1265ea297a42fbb5
Ryan Dalal <ryanadalal@gmail.com>
2022-07-24 12:34:06 -0400

    Add files via upload

--- milk2.py
+++ milk2.py
@@ -8,46 +8,53 @@ TASK: milk2
 fin = open('milk2.in', 'r')
 fout = open('milk2.out', 'w')
 
-words = fin.read().split()
+words = list(map(int, fin.read().split()))
 
 n = int(words[0])
-start = list(map(int, words[1::2]))
-end = list(map(int, words[2::2]))
+starts = [words[1]]
+ends = [words[2]]
+for i in range(3, len(words) - 1, 2):
+    for j in range(len(starts)):
+        if words[i] < starts[j]:
+            starts.insert(j, words[i])
+            ends.insert(j, words[i + 1])
+            break
+        elif j == len(starts) - 1:
+            starts.append(words[i])
+            ends.append(words[i + 1])
+
+print(starts)
+print(ends)
+
+removes = []
+removee = []
+for i in range(len(starts)):
+    for j in range(i + 1, len(starts)):
+        if ends[i] >= starts[j]:
+            print('removing', min(ends[i], ends[j]),starts[j])
+            print('i,j', i,j)
+            removee.append(min(ends[i], ends[j]))
+            removes.append(starts[j])
+
+print(removes)
+print(removee)
+
+for i in range(len(removes)):
+    starts.remove(removes[i])
+    ends.remove(removee[i])
+
+print(starts)
+print(ends)
 
 maxidle = 0
 maxcontinuous = 0
-ranges = [] #add each one to a range
-ranges.append((start[0], end[0]))
-
-for i in range(1, n):
-    print(i)
-    for j in range(len(ranges)):
-        if ranges[j][0] <= end[i] and ranges[j][1] >= end[i] or ranges[j][0] <= start[i] and ranges[j][1] >= start[i]:
-            ranges[j] = (min(start[i], ranges[j][0]), max(ranges[j][1], end[i]))
-            continue
-        elif start[i] >= ranges[j][0] and end[i] <= ranges[j][1]:
-            continue
-        else:
-            add = True
-            for k in range(len(ranges)):
-                if start[i] == ranges[k][0] and end[i] == ranges[k][1]:
-                    add = False
-            if add: ranges.append((start[i], end[i]))
-
-finalranges = []
-for i in range(len(ranges)):
-    add = True
-    for j in range(len(ranges)):
-        if i != j and ranges[i][0] > ranges[j][0] and ranges[i][1] < ranges[j][1]:
-            add = False
-    if add: finalranges.append(ranges[i])
 
 
-for i in range(len(finalranges)):
-    if finalranges[i][1] - finalranges[i][0] > maxcontinuous:
-        maxcontinuous = finalranges[i][1] - finalranges[i][0]
-    if i + 1 < len(finalranges) and finalranges[i + 1][0] - finalranges[i][1] > maxidle:
-        maxidle = finalranges[i + 1][0] - finalranges[i][1]
+for i in range(len(starts)):
+    if ends[i] - starts[i] > maxcontinuous:
+        maxcontinuous = ends[i] - starts[i]
+    if i + 1 < len(starts) and starts[i + 1] - ends[i] > maxidle:
+        maxidle = starts[i + 1] - ends[i]
 
 fout.write(str(maxcontinuous) + ' ' + str(maxidle) + '\n')
 
